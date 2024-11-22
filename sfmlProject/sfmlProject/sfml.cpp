aaa298175365e4aaf6e59fb1a230a72dbd6137f4
Mishy0485 <ayite.julia06@gmail.com>
2024-11-22 10:21:06 +0100

    creation

---
+++ sfmlProject/sfmlProject/sfml.cpp
@@ -0,0 +1,13 @@
+#include<SFML/Graphics.hpp>
+using namespace sf;
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// création de fenetre
+	// activité de la fenetre : ?
+	// creation de boutons
+	// creation d'edit
+	// dessin ou jeu ou texte
+}
