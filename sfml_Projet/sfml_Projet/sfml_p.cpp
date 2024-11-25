cb8640934b4a44e5720640dd560c90915a987a29
Mishy0485 <ayite.julia06@gmail.com>
2024-11-25 10:29:19 +0100

    edit

--- sfml_Projet/sfml_Projet/sfml_p.cpp
+++ sfml_Projet/sfml_Projet/sfml_p.cpp
@@ -3,57 +3,96 @@ using namespace sf;
 #include<iostream>
 using namespace std;
 
-// id e jeu de rapidit  et al atoire un piano tiles?
-// conteur de vie
+// idée : jeu de rapidite et aleatoire un piano tiles?
+// compteur de vie
 
 int main()
 {
-	// cr ation de fenetre
+	// creation de fênètre
 	RenderWindow window(VideoMode(560, 600), "Project SFML");
 	
-	// Création d'apparition rectangle
+	// Création d'apparition rectanglulaire
+
+	RectangleShape rec(Vector2f(80.f, 80.f));
+	rec.setFillColor(Color::White); 
+	rec.setPosition(60.f, 60.f);    
 
 	RectangleShape rec1(Vector2f(80.f, 80.f));
-	rec1.setFillColor(Color::White); // Remplissage rouge
-	rec1.setPosition(60.f, 60.f);    // Position centrale
+	rec1.setFillColor(Color::White); 
+	rec1.setPosition(180.f, 60.f);    
 
 	RectangleShape rec2(Vector2f(80.f, 80.f));
-	rec2.setFillColor(Color::White); // Remplissage rouge
-	rec2.setPosition(180.f, 60.f);    // Position centrale
+	rec2.setFillColor( Color::White); 
+	rec2.setPosition (300.f, 60.f);    
 
 	RectangleShape rec3(Vector2f(80.f, 80.f));
-	rec3.setFillColor( Color::White); // Remplissage rouge
-	rec3.setPosition (300.f, 60.f);    // Position centrale
+	rec3.setFillColor(Color::White); 
+	rec3.setPosition(420.f, 60.f);    
 
-	RectangleShape rec4(Vector2f(80.f, 80.f));
-	rec4.setFillColor(Color::White); // Remplissage rouge
-	rec4.setPosition(420.f, 60.f);    // Position centrale
 
-	// 4 touches : Fl ches
+	// Images : 4 flèches
+	
+	Texture texture;
+	if (!texture.loadFromFile("fleche gauche1.jpg")) {
+		return -1; // Erreur si le fichier est introuvable
+	}
+	Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.setPosition(Vector2f(60.f, 60.f));
+
+	Texture texture1;
+	if (!texture1.loadFromFile("fleche haut1.jpg")) {
+		return -1; // Erreur si le fichier est introuvable
+	}
+	Sprite sprite1;
+	sprite.setTexture(texture1);
+	sprite.setPosition(Vector2f(180.f, 60.f));
+
+	Texture texture2;
+	if (!texture2.loadFromFile("fleche droite1.jpg")) {
+		return -1; // Erreur si le fichier est introuvable
+	}
+	Sprite sprite2;
+	sprite.setTexture(texture2);
+	sprite.setPosition(Vector2f(300.f, 60.f));
+
+	Texture texture3;
+	if (!texture3.loadFromFile("fleche bas1.jpg")) {
+		return -1; // Erreur si le fichier est introuvable
+	}
+	Sprite sprite3;
+	sprite.setTexture(texture3);
+	sprite.setPosition(Vector2f(420.f, 60.f));
+
+
+
+
+
+	// 4 touches : Fleches
 
 	if (Keyboard::isKeyPressed(Keyboard::Left))
 	{
-		// a voir ce qui se passe :  tincelle ?
+		// a voir ce qui se passe :  etincelle ?
 	}
 
 	if (Keyboard::isKeyPressed(Keyboard::Right))
 	{
-		// a voir ce qui se passe :  tincelle ?
+		// a voir ce qui se passe :  etincelle ?
 	}
 
 	if (Keyboard::isKeyPressed(Keyboard::Up))
 	{
-		// a voir ce qui se passe :  tincelle ?
+		// a voir ce qui se passe :  etincelle ?
 	}
 
 	if (Keyboard::isKeyPressed(Keyboard::Down))
 	{
-		// a voir ce qui se passe :  tincelle ?
+		// a voir ce qui se passe :  etincelle ?
 	}
 
 
-	// apparition al atoire d'une des 4 touches sur la fen tre
-	// touch  la touche indiqu e dans le temps imparti 
+	// apparition aleatoire d'une des 4 touches sur la fenetre
+	// touche la touche indiquee dans le temps imparti 
 
 	while (window.isOpen())
 	{
@@ -65,12 +104,27 @@ int main()
 		}
 
 		window.clear();
+		window.draw(rec);
+		window.draw(sprite);
 		window.draw(rec1);
+		window.draw(sprite1);
 		window.draw(rec2);
+		window.draw(sprite2);
 		window.draw(rec3);
-		window.draw(rec4);
+		window.draw(sprite3);
 		window.display();
 	}
 
 	return 0;
 }
+
+// menu de départ avec start ou exit? a faire a la fin
+// fleche apparait aleatoirement a un emplacement donné
+// si le touche coresspondante est appuyé score + 5
+// sinon compteur de vie diminue ( 5 vies )
+// quand perdu pop_up avec le score / si gagné avgec le score et le nb de vies restantes 
+
+/* 
+for 
+
+if */
